0e513a055fbe8ec33212bbf137155f2b79f7eabe
gaimn <104718139+gaimn@users.noreply.github.com>
2023-09-24 00:52:08 +0100

    Update layers.js
    haha

--- js/layers.js
+++ js/layers.js
@@ -24,6 +24,10 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    branches: [
+	"energize",
+	"reaction",
+    ],
     layerShown(){return true}
 })
 addLayer("e", {
@@ -38,7 +42,32 @@ addLayer("e", {
     requires: new Decimal(25), // Can be a function that takes requirement increases into account
     resource: "electricity", // Name of prestige currency
     baseResource: "prestige points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.p.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true}
+})
+addLayer("e", {
+    name: "reaction", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#EBDD81",
+    requires: new Decimal(500), // Can be a function that takes requirement increases into account
+    resource: "electricity", // Name of prestige currency
+    baseResource: "prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
