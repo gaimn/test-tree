62b7a8c4b3afcdc9a364d61a508a0b252b3be28e
gaimn <104718139+gaimn@users.noreply.github.com>
2023-10-10 17:31:31 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -46,6 +46,7 @@ function getPointGen() {
 	if (hasUpgrade('p', 11)) gain = gain.times(3)
 	if (hasUpgrade('p', 12)) gain = gain.times(2)
 	if (hasUpgrade('p', 13)) gain = gain.times(upgradeEffect('p', 13))
+	if (hasUpgrade('e', 12)) gain = gain.times(upgradeEffect('e', 12))
 	return gain
 }
 
