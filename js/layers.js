e3da06fd318240d8cd626b62bfef17fda581a830
gaimn <104718139+gaimn@users.noreply.github.com>
2025-09-20 21:31:44 +0100

    added an extremely important feature

--- js/layers.js
+++ js/layers.js
@@ -94,7 +94,7 @@ addLayer("e", {
     requires: new Decimal(25), // Can be a function that takes requirement increases into account
     resource: "electricity", // Name of prestige currency
     baseResource: "prestige points", // Name of resource prestige is based on
-    baseAmount() {return player.p.points}, // Get the current amount of baseResource
+    baseAmount() {return player.e.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -137,12 +137,38 @@ addLayer("r", {
     requires: new Decimal(500), // Can be a function that takes requirement increases into account
     resource: "booster", // Name of prestige currency
     baseResource: "prestige points", // Name of resource prestige is based on
-    baseAmount() {return player.p.points}, // Get the current amount of baseResource
+    baseAmount() {return player.r.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
 		if (hasUpgrade('p', 23)) mult = mult.times(upgradeEffect('p', 23))
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true}
+})
+
+addLayer("x", {
+    name: "unknown", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "⊘", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#FF0000",
+    requires: new Decimal("6eee7"), // Can be a function that takes requirement increases into account
+    resource: "unknown", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.x.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
