fbd7c12bf0f4b068ee94b3565a8b9ff3d0277967
gaimn <104718139+gaimn@users.noreply.github.com>
2025-09-20 18:01:02 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -58,7 +58,7 @@ addLayer("e", {
     symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "#EBDD81",
@@ -99,7 +99,7 @@ addLayer("r", {
     symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "#6E9E4E",
